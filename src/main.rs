05dd046f87cb7baf57ed6a8eb00b083a65164ab1
Prasanth Thomas Shaji <prasanthshaji@icloud.com>
2026-06-30 21:54:03 +0200

    Read line and echo

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
-//! Super special program
+//! Starter REPL
 
-use std::cmp::Ordering;
 use std::io;
 
-/// Simple program to guess a number between 0 and a 100
+/// Simple echos only
 fn main() {
-    let pick: u8 = rand::random_range(0..=100);
-    println!("Sh!!! Picked {pick}");
-    println!("Please take make a guess");
-
     loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read a number");
-        let guess: u8 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Um, maybe guess again");
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => {
+                break
+            }
+            Ok(_) => {
+                match input.trim() {
+                    "exit" => break,
+                    _ => println!("{input}"),
+                }
+            }
+            Err(error) => {
+                println!("Um {error}, maybe try again");
                 continue
             }
-        };
-
-        match guess.cmp(& pick) {
-            Ordering::Less => println!("Your guess is too low"),
-            Ordering::Greater => println!("Your guess is too high"),
-            Ordering::Equal => {println!("Exactomondo!"); break;}
         }
     }
 }
